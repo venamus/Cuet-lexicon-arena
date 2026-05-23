d22c3bec8e448ce7349af8d10973f5fe421dfefa
venamus <ayush36630@gmail.com>
2026-05-23 16:00:52 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CUETVocabArena from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <CUETVocabArena />
+);
